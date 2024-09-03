3313f557d9071a349be9c497c6cd7b1cd4be523f
Jorge Flores <jorge@entidad.io>
2024-09-03 13:00:27 -0700

    Downgraded agent to Credo v0.5.6, upgraded DIDComm Survey and DIDComm RPC modules
    Signed-off-by: Jorge Flores <jorge@entidad.io>

--- source/prj/android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
+++ source/prj/android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
@@ -16,23 +16,7 @@ import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 import java.util.List;
 
-import android.content.Context; // make sure you are not importing multiple times
-import android.content.BroadcastReceiver;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build;
-import org.jetbrains.annotations.Nullable;
-
 public class MainApplication extends MendixReactApplication {
-
-    @Override
-    public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
-        if (Build.VERSION.SDK_INT >= 34 && getApplicationInfo().targetSdkVersion >= 34) {
-            return super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
-        } else {
-            return super.registerReceiver(receiver, filter);
-        }
-    }
     @Override
     public boolean getUseDeveloperSupport() {
         return false;
